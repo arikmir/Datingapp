a6c2e75820429cb92107a94d00215de46a015d04
Arik Mir <mirarikintenam@gmail.com>
2021-06-01 21:31:42 +1000

    User API Skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        //GET- api/users/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = _context.Users.ToListAsync();
+            return await users;
+        }
+        //GET- api/users/getuser/
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+    }
+}
