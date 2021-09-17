f1dbfc0a981b0a9a48856e433863c1092c5a5c86
Arik Intenam Mir <arik@canariatechnologies.com>
2021-09-18 00:02:17 +1000

    fromLaptop

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -20,7 +20,7 @@ namespace API.Extensions
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
-            return services;
+            return services; 
         }
     }
 }
