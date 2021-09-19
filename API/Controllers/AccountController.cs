adb4e3b91ba40a206a5b6f9412781e478e033146
Arik Mir <arik@canariatechnologies.com>
2021-09-19 15:32:53 +1000

    photo-functionality-done

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -53,6 +53,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
+            .Include(p=> p.Photos)
             .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
             if (user == null)
@@ -73,7 +74,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
             };
         }
 
