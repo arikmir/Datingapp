5d673ebf3e66ccd1a308fde386d9dd46e1049194
Arik Mir <arik@canariatechnologies.com>
2021-09-29 20:56:57 +1000

    server-side-pagination-done

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,6 +12,7 @@ using AutoMapper;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using API.Extensions;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -31,9 +32,11 @@ namespace API.Controllers
         //GET- api/users/
         [HttpGet]
 
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
 
             return Ok(users);
         }
